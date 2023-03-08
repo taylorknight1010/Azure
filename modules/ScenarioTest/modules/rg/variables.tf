152a1c67eadcc3a0f85d82c5cd3e8ceef4c3c701
taylorknight1010 <65223527+taylorknight1010@users.noreply.github.com>
2023-03-08 23:06:55 +0000

    Create variables.tf

---
+++ modules/ScenarioTest/modules/rg/variables.tf
@@ -0,0 +1,14 @@
+variable "resource_group_name" {
+  description = "The name which should be used for this resource group."
+  type = string
+}
+
+variable "location" {
+  description = "The name which should be used for this resource group."
+  type = string
+}
+
+variable "tags" {
+  description = "The name which should be used for this resource group."
+  type = map
+}
