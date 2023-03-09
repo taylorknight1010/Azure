d7c033c6dbc8234fe22392a1de209055ea994492
taylorknight1010 <65223527+taylorknight1010@users.noreply.github.com>
2023-03-09 20:11:54 +0000

    Update vm.tf

--- modules/ScenarioTest/workingdir/vm.tf
+++ modules/ScenarioTest/workingdir/vm.tf
@@ -1,1 +1,14 @@
-
+module "vm" {
+  source = "../modules/vm"
+  
+  nic = local.hubvnet
+  location = local.location
+  vm = local.coresubnet
+  resource_group_name = module.rg.resource_group_name
+  coresubnet = module.vnet.coresubnet
+  
+  tags = {
+    Terraform   = "true"
+    Environment = "prod"
+  }
+}
