72ec459f682d9ef6d9d7125ed1e6811081979cfc
taylorknight1010 <65223527+taylorknight1010@users.noreply.github.com>
2023-03-09 22:38:24 +0000

    Update vm.tf

--- modules/ScenarioTest/workingdir/vm.tf
+++ modules/ScenarioTest/workingdir/vm.tf
@@ -7,12 +7,12 @@ module "vm" {
   resource_group_name = module.rg.resource_group_name
   coresubnet = module.vnet.coresubnet
   hubvnet = module.vnet.hubvnet
-  publisher = local.vm.source_image_reference.publisher
-  offer     = local.vm.source_image_reference.offer
-  sku       = local.vm.source_image_reference.sku
-  osversion   = local.vm.source_image_reference.verison  
-  caching              = local.vm.os_disk.caching
-  storage_account_type = local.vm.os_disk.storage_account_type  
+  publisher = local.vm.publisher
+  offer     = local.vm.offer
+  sku       = local.vm.sku
+  osversion   = local.vm.verison  
+  caching              = local.vm.caching
+  storage_account_type = local.vm.storage_account_type  
   size                = local.vm.size
   subnet_id                     = module.vnet.coresubnet.id  
   
