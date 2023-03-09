112b2672c796d36ac75dc1bbc9e2b0012c2f2c4b
taylorknight1010 <65223527+taylorknight1010@users.noreply.github.com>
2023-03-09 21:08:21 +0000

    Update main.tf

--- modules/ScenarioTest/modules/vm/main.tf
+++ modules/ScenarioTest/modules/vm/main.tf
@@ -6,7 +6,7 @@ resource "azurerm_network_interface" "nic" {
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = var.coresubnet.name
+    subnet_id                     = var.coresubnet.id
     private_ip_address_allocation = "Dynamic"
   }
 }
