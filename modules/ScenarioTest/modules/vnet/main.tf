2cde1ef40251c96a0ca4b9d4897e45983ccae0be
taylorknight1010 <65223527+taylorknight1010@users.noreply.github.com>
2023-03-09 19:16:56 +0000

    Update main.tf

--- modules/ScenarioTest/modules/vnet/main.tf
+++ modules/ScenarioTest/modules/vnet/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_virtual_network" "hubvnet" {
   name                = var.hubvnet.id
   address_space       = var.hubvnet.address_space
   location            = var.hubvnet.location
-  resource_group_name = module.resource_group.resource_group_name
+  resource_group_name = var.resource_group_name.name
   tags                = var.tags  
 }
 
