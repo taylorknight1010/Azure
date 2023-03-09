76cddaaac6e9f325bbfba542e897dd4a6032840c
taylorknight1010 <65223527+taylorknight1010@users.noreply.github.com>
2023-03-09 20:06:21 +0000

    Update variables.tf

--- modules/ScenarioTest/modules/vm/variables.tf
+++ modules/ScenarioTest/modules/vm/variables.tf
@@ -1,1 +1,23 @@
+variable "nic" {
+  type = any
+}
+
+variable "vm" {
+  type = any
+}
+
+variable "location" {
+  description = "The location which should be used for this virtual network."
+  type = string
+}
+
+variable "tags" {
+  description = "The tags which should be used for this virtual network."
+  type = map
+}
+
+variable "resource_group_name" {
+  description = "The location which should be used for this virtual network."
+  type = any
+}
 
