359624a0d8014577df89d2e4c5517bb21004d266
taylorknight1010 <65223527+taylorknight1010@users.noreply.github.com>
2023-03-09 22:29:35 +0000

    Update vm.tf

--- modules/ScenarioTest/workingdir/vm.tf
+++ modules/ScenarioTest/workingdir/vm.tf
@@ -7,6 +7,14 @@ module "vm" {
   resource_group_name = module.rg.resource_group_name
   coresubnet = module.vnet.coresubnet
   hubvnet = module.vnet.hubvnet
+  publisher = local.vm.source_image_reference.publisher
+  offer     = local.vm.source_image_reference.offer
+  sku       = local.vm.source_image_reference.sku
+  version   = local.vm.source_image_reference.verison  
+  caching              = local.vm.os_disk.caching
+  storage_account_type = local.vm.os_disk.storage_account_type  
+  size                = local.vm.size
+  subnet_id                     = module.vnet.coresubnet.id  
   
   tags = {
     Terraform   = "true"
