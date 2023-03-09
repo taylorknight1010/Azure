1408ee4d1965a065c0f35c7be1859514a8ae913e
taylorknight1010 <65223527+taylorknight1010@users.noreply.github.com>
2023-03-09 20:23:35 +0000

    Update outputs.tf

--- modules/ScenarioTest/modules/vm/outputs.tf
+++ modules/ScenarioTest/modules/vm/outputs.tf
@@ -1,9 +1,9 @@
 output "nic" {
   description = "virtual network created"
-  value       = azurerm_virtual_network.hubvnet.name
+  value       = azurerm_virtual_network.hubvnet.id
 }
 
 output "vm" {
   description = "subnet created"
-  value       = azurerm_subnet.coresubnet.name
+  value       = azurerm_subnet.coresubnet.id
 }
