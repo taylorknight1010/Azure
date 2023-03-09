1d6959d1076880a096a912db0e9e551e2c31c62c
taylorknight1010 <65223527+taylorknight1010@users.noreply.github.com>
2023-03-09 15:10:49 +0000

    Create locals.tf

---
+++ modules/ScenarioTest/workingdir/locals.tf
@@ -0,0 +1,26 @@
+
+locals {
+
+rg_name = "rg-adds-dcs-uks"
+
+location = "uksouth"
+
+vm_names = {
+  DC01 = { 
+    ip_address = "10.245.100.10"
+    sku = "2019-Datacenter"
+    data_disk = false
+    
+  }
+  DC02 = { 
+    ip_address = "10.245.100.20"
+    sku = "2019-Datacenter"
+    data_disk = false
+    
+  }
+  
+  
+  
+  
+}
+}
