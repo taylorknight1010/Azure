6c73fb32b983db6a369668fc051f967dc2c6542b
taylorknight1010 <65223527+taylorknight1010@users.noreply.github.com>
2023-03-09 20:20:57 +0000

    Update vm.tf

--- modules/ScenarioTest/workingdir/vm.tf
+++ modules/ScenarioTest/workingdir/vm.tf
@@ -6,6 +6,7 @@ module "vm" {
   vm = local.coresubnet
   resource_group_name = module.rg.resource_group_name
   coresubnet = module.vnet.coresubnet
+  hubvnet = module.vnet.hubvnet
   
   tags = {
     Terraform   = "true"
