d50430b08b76c6dbb31d3ba0d94092982e693caf
taylorknight1010 <65223527+taylorknight1010@users.noreply.github.com>
2023-03-10 00:23:16 +0000

    Update variables.tf

--- modules/ScenarioTest/modules/vm/variables.tf
+++ modules/ScenarioTest/modules/vm/variables.tf
@@ -1,45 +1,70 @@
-variable "vm" {
-  type = object({
-    id                  = string
-    publisher           = string
-    offer               = string
-    sku                 = string
-    caching             = string
-    storage_account_type = string
-    size                = string
-    osversion           = string
-  })
+variable "rg_name" {
+  description = "Name of the resource group."
+}
+
+variable "location" {
+  description = "Location for all resources."
+}
+
+variable "hubvnet_id" {
+  description = "ID of the hub virtual network."
+}
+
+variable "hubvnet_address_space" {
+  description = "Address space of the hub virtual network."
+  type = list(string)
+}
+
+variable "coresubnet_id" {
+  description = "ID of the core subnet."
+}
+
+variable "coresubnet_address_prefixes" {
+  description = "Address prefixes of the core subnet."
+  type = list(string)
 }
 
 variable "nic" {
-  type = any
+  description = "ID of the NIC."
 }
 
-variable "hubvnet" {
-  type = string
+variable "vm_id" {
+  description = "ID of the virtual machine."
 }
 
-variable "coresubnet" {
-  type = string
+variable "vm_size" {
+  description = "Size of the virtual machine."
 }
 
-
-variable "location" {
-  description = "The location which should be used for this virtual network."
-  type = string
+variable "vm_caching" {
+  description = "Caching type for the virtual machine OS disk."
 }
 
-variable "tags" {
-  description = "The tags which should be used for this virtual network."
-  type = map
+variable "vm_storage_account_type" {
+  description = "Storage account type for the virtual machine OS disk."
 }
 
-variable "resource_group_name" {
-  description = "The location which should be used for this virtual network."
-  type = any
+variable "vm_publisher" {
+  description = "Publisher of the virtual machine image."
 }
 
-variable "osversion" {
- default = "latest" 
+variable "vm_offer" {
+  description = "Offer of the virtual machine image."
 }
 
+variable "vm_sku" {
+  description = "SKU of the virtual machine image."
+}
+
+variable "vm_osversion" {
+  description = "Operating system version of the virtual machine image."
+}
+
+variable "vm_names" {
+  description = "Map of virtual machine names to their configurations."
+  type = map(object({
+    ip_address = string
+    sku = string
+    data_disk = bool
+  }))
+}
