747a5573f5453becf6e7b06466870e114e5bf67f
taylorknight1010 <65223527+taylorknight1010@users.noreply.github.com>
2023-03-13 23:38:16 +0000

    Update vm.tf

--- modules/ScenarioTest/workingdir/vm.tf
+++ modules/ScenarioTest/workingdir/vm.tf
@@ -2,8 +2,8 @@ module "vm" {
   source = "../modules/vm"
   resource_group_name = module.rg.resource_group_name
   location = var.location
-  nic_id = var.nic_id
-  coresubnet = var.coresubnet
+  nic_id = module.vnet.nic_id
+  coresubnet = module.vnet.coresubnet
   vm_names = var.vm_names
   vm_size = var.vm_size
   storage_account_type = var.storage_account_type
