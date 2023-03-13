8e2bd6d9ebd0a1422e0eaf122036eaffc549de3f
taylorknight1010 <65223527+taylorknight1010@users.noreply.github.com>
2023-03-13 23:35:31 +0000

    Update outputs.tf

--- modules/ScenarioTest/modules/vm/outputs.tf
+++ modules/ScenarioTest/modules/vm/outputs.tf
@@ -1,9 +1,9 @@
 output "nic" {
   description = "NIC for VM created"
-  value       = azurerm_network_interface.nic.name
+  value       = azurerm_network_interface.nic_id.name
 }
 
 output "vm" {
   description = "virtual machine created"
-  value       = azurerm_windows_virtual_machine.vm.name
+  value       = azurerm_windows_virtual_machine.vm_names.name
 }
