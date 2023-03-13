37abb4604079c6386d5fe44e69e40928ba87d00c
taylorknight1010 <65223527+taylorknight1010@users.noreply.github.com>
2023-03-13 22:56:06 +0000

    Update main.tf

--- modules/ScenarioTest/modules/vnet/main.tf
+++ modules/ScenarioTest/modules/vnet/main.tf
@@ -1,15 +1,15 @@
 resource "azurerm_virtual_network" "hubvnet" {
-  name                = "${var.hubvnet.id}"
-  address_space       = "${var.hubvnet.address_space}"
-  location            = "${var.hubvnet.location}"
+  name                = var.hubvnet
+  address_space       = var.address_space
+  location            = var.location
   resource_group_name = var.resource_group_name
   tags                = var.tags  
 }
 
 resource "azurerm_subnet" "coresubnet" {
-  name                 = "${var.coresubnet.id}"
+  name                 = var.coresubnet
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.hubvnet.name
-  address_prefixes     = "${var.coresubnet.address_prefixes}"
+  address_prefixes     = var.address_prefixes
 }
 
