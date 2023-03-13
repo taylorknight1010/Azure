b856d27dd6b03d40d60e6c57a0e90e38c4c3eb52
taylorknight1010 <65223527+taylorknight1010@users.noreply.github.com>
2023-03-13 23:30:23 +0000

    Update vm.tf

--- modules/ScenarioTest/workingdir/vm.tf
+++ modules/ScenarioTest/workingdir/vm.tf
@@ -1,44 +1,19 @@
 module "vm" {
   source = "../modules/vm"
-  rg_name = module.rg.resource_group_name
+  resource_group_name = module.rg.resource_group_name
   location = var.location
-
-  hubvnet = {
-    id = module.vnet.hubvnet_id
-    address_space = module.vnet.hubvnet.address_space
-    location = var.location
-  }
-
-  coresubnet = {
-    id = module.vnet.coresubnet.id
-    address_prefixes = module.vnet.coresubnet.address_prefixes
-  }
-
-  nic = {
-    id = var.nic_id
-    tags = {
-    Terraform   = "true"
-    Environment = "prod"
-  }
-  }
-
-  vm = {
-    id = var.vm_id
-    location = var.location
-    tags = {
-    Terraform   = "true"
-    Environment = "prod"
-  }
-    size = var.vm_size
-    caching = var.vm_caching
-    storage_account_type = var.vm_storage_account_type
-    publisher = var.vm_publisher
-    offer = var.vm_offer
-    sku = var.vm_sku
-    osversion = var.vm_osversion
-  }
-
+  nic_id = var.nic_id
+  coresubnet = var.coresubnet
   vm_names = var.vm_names
-}
+  vm_size = var.vm_size
+  storage_account_type = var.storage_account_type
+  caching = var.caching
+  managed_disk_type = var.managed_disk_type
+  publisher = var.publisher
+  sku = var.sku
+  osversion = var.osversion
+  offer = var.offer
+
+    }
  
 
