d82418a15227af73e539414ee10d8acf3e61665a
taylorknight1010 <65223527+taylorknight1010@users.noreply.github.com>
2023-03-14 01:24:15 +0000

    Update outputs.tf

--- modules/privatedns/modules/rg/outputs.tf
+++ modules/privatedns/modules/rg/outputs.tf
@@ -1,4 +1,12 @@
+#output "resource_group_name" {
+ # description = "resource group for dcs"
+  #value       = azurerm_resource_group.rg.name
+#}
+
 output "resource_group_name" {
-  description = "resource group for dcs"
-  value       = azurerm_resource_group.rg.name
+  description = "The name of the resource group"
+  value = {
+    uksouth = azurerm_resource_group.rg.uksouth.name
+    ukwest  = azurerm_resource_group.rg.ukwest.name
+  }
 }
