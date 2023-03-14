e1264a239a6cc4e614c6049a12b2184002edc8e7
taylorknight1010 <65223527+taylorknight1010@users.noreply.github.com>
2023-03-14 00:41:30 +0000

    Update variables.tf

--- modules/privatedns/modules/privatedns/variables.tf
+++ modules/privatedns/modules/privatedns/variables.tf
@@ -1,7 +1,14 @@
-variable {
-  dns_zone = var.dns_zone
-  }
+variable "dns_zone" {
+  description = "The name which should be used for this resource group."
 
-variable {
- resource_group_name = var.resource_group_name 
-  }
+}
+
+variable "resource_group_name" {
+  description = "The name which should be used for this resource group."
+
+}
+
+variable "tags" {
+  description = "The name which should be used for this resource group."
+
+}
