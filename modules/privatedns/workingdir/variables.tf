0d780853762aea8be857005bc81780e444d3e6fb
taylorknight1010 <65223527+taylorknight1010@users.noreply.github.com>
2023-03-14 00:46:46 +0000

    Create variables.tf

---
+++ modules/privatedns/workingdir/variables.tf
@@ -0,0 +1,19 @@
+variable "dns_zone" {
+  description = "The name which should be used for this resource group."
+
+}
+
+variable "resource_group_name" {
+  description = "The name which should be used for this resource group."
+
+}
+
+variable "tags" {
+  description = "The name which should be used for this resource group."
+
+}
+
+variable "location" {
+  description = "The name which should be used for this resource group."
+  type = string
+}
