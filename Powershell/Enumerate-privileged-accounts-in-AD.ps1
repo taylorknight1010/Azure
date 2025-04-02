8480ad9a2b3e4359c77cc6cc47baa40156852e7c
taylorknight1010 <65223527+taylorknight1010@users.noreply.github.com>
2025-04-02 10:50:49 +0100

    Update Enumerate-privileged-accounts-in-AD.ps1

--- Powershell/Enumerate-privileged-accounts-in-AD.ps1
+++ Powershell/Enumerate-privileged-accounts-in-AD.ps1
@@ -26,3 +26,31 @@ $ADSI.psbase.get_ObjectSecurity().getAccessRules($true, $true,[system.security.p
 # To discover all the members that are part of a protected group. Run the following command: 
 
 ([adsisearcher]'(&(objectClass=user)(adminCount=1))').FindAll()
+
+# Unconstatined user accounts configured
+
+([adsisearcher]'(&(objectCategory=user)(userAccountControl:1.2.840.113556.1.4.803:=524288))').FindAll() 
+
+# Get membership from privileged groups
+# DnsAdmins
+([adsisearcher]'(memberOf=cn=DnsAdmins,CN=Users,dc=fabrikam,dc=com)').FindAll() 
+# Do the same for the rest of the priv groups:
+# Domain Admins 
+# Enterprise Admins 
+# Schema Admins 
+# Administrators 
+# Account Operators 
+# Backup Operators 
+# Server Operators 
+# Print Operators 
+# Cert Publishers 
+# Group Policy Creator Owners
+
+# Get Local Admin membership from Tier 0 servers
+# AD Connect, PKI, ADFS etc - Tier 0 examples
+$LocalGroup =[ADSI]"WinNT://Server/Administrators" 
+$UserNames = @($LocalGroup.psbase.Invoke("Members")) 
+$UserNames | foreach {$_.GetType().InvokeMember("Name",'GetProperty', $null, $_, $null)} 
+
+
+
